ac6d4dddfe1fdce0c90f152c91c253c1a1e0d43a
Ly Nguyen <lynguyen237@Lys-MacBook-Pro.local>
2021-04-17 10:25:07 -0700

    Break down the recursion function of the count_islands problem

--- count_islands.py
+++ count_islands.py
@@ -31,6 +31,8 @@ n == grid[i].length
 grid[i][j] is '0' or '1'.
 '''
 
+#==== Solution 1, recursion ====
+
 def count_islands(matrix):
     
     def dfs(i, j, matrix):
@@ -67,3 +69,33 @@ grid2 = [
 
 print(count_islands(grid1))
 print(count_islands(grid2))
+
+#==== Solution 1B - slightly different way to unpack recursion ====
+# Count islands
+def count_islands2(matrix):
+
+  count = 0
+  
+  for r in range(len(matrix)):
+    for c in range(len(matrix[r])):
+      if matrix[r][c] == "1":
+        dfs(r,c, matrix)
+        count += 1
+  
+  return count
+
+# DFS
+def dfs(r, c, matrix):
+
+  if r < 0 or r >= len(matrix) or c < 0 or c >= len(matrix[0]): # End the current dfs if the r & c are out of bounds
+    return
+  
+  if matrix[r][c] == "X" or matrix[r][c] == "0": # End the current dfs if the cell is either 0 or has been checked before
+    return
+  
+  matrix[r][c] = "X"
+  
+  dfs(r, c-1, matrix) #left
+  dfs(r, c+1, matrix) #right
+  dfs(r-1, c, matrix) #up
+  dfs(r+1, c, matrix) #down
